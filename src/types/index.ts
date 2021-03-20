c9ab8eff1e1ac3f24ce71800ba4a051e7ca66013
Josh Grant <josh@boyne.io>
2021-03-20 17:27:33 -0500

    eemagi_5 Create types for User and Photo

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type User = {
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+}
+
+export type Photo = {
+    id: number;
+    title: string;
+    caption: string;
+}
