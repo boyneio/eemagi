a5ff6397e236eb23dd0103da41d371b576b53c9e
Josh Grant <josh@boyne.io>
2021-03-27 07:32:45 -0500

    export the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 import { Photo, User } from './types';
+import API from './api';
 
-const base = process.env.REACT_APP_API_URL;
-console.log(base);
-
-const api = axios.create({baseURL: base});
 
 function App() {
   const [photos, setPhotos] = React.useState<Photo[]>([]);
   React.useEffect(() => {
-    api.get('photos').then(res => setPhotos(res.data));
+    API.get('photos').then(res => setPhotos(res.data));
   }, []);
   return (
     <div className="App">
